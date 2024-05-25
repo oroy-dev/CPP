08a1b63943150a3b1fc87ed5492d5e5e9c3af8c1
Olivier Roy <120348668+oroy-dev@users.noreply.github.com>
2024-05-25 16:26:56 -0400

    few tweaks

--- CPP-Module-05/ex03/main.cpp
+++ CPP-Module-05/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: olivierroy <olivierroy@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 16:09:26 by oroy              #+#    #+#             */
-/*   Updated: 2024/05/24 16:50:57 by oroy             ###   ########.fr       */
+/*   Updated: 2024/05/25 16:22:25 by olivierroy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,28 @@ int	main(void)
 	Intern		someRandomIntern;
 	Bureaucrat	squareEnix("SquareEnix", 2);
 
-	// AForm	*none;
-	// none = someRandomIntern.makeForm("", "");
-	// squareEnix.signForm(*none);
-	// squareEnix.executeForm(*none);
+	std::cout << std::endl;
 
 	AForm	*ppf;
 	ppf = someRandomIntern.makeForm("presidential pardon", "Octo");
 	squareEnix.signForm(*ppf);
 	squareEnix.executeForm(*ppf);
 
+	std::cout << std::endl;
+
 	AForm	*rrf;
 	rrf = someRandomIntern.makeForm("robotomy request", "Path");
 	squareEnix.signForm(*rrf);
 	squareEnix.executeForm(*rrf);
 
+	std::cout << std::endl;
+
 	AForm	*scf;
 	scf = someRandomIntern.makeForm("shrubbery creation", "Traveler");
 	squareEnix.signForm(*scf);
 	squareEnix.executeForm(*scf);
+
+	std::cout << std::endl;
 
 	delete ppf;
 	delete rrf;
