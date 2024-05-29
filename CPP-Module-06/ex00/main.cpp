74fe92f5de186dc0f16a81e3051a7a71db1b9566
Olivier Roy <120348668+oroy-dev@users.noreply.github.com>
2024-05-28 23:32:39 -0400

    stuff

--- CPP-Module-06/ex00/main.cpp
+++ CPP-Module-06/ex00/main.cpp
@@ -6,12 +6,18 @@
 /*   By: olivierroy <olivierroy@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 18:14:05 by oroy              #+#    #+#             */
-/*   Updated: 2024/05/27 22:07:16 by olivierroy       ###   ########.fr       */
+/*   Updated: 2024/05/28 23:32:20 by olivierroy       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+
+#define _STRING	-1
+#define	_CHAR	0
+#define	_INT	1
+#define	_FLOAT	2
+#define	_DOUBLE	3
 
 static void	print()
 {
@@ -26,23 +32,47 @@ static void	convert(std::string str)
 
 }
 
-static void	detectType(std::string str)
+static int	correctType(std::string::size_type i, bool comma, bool f, bool digit)
+{
+	if (!digit && i == 0)
+		return (_CHAR);
+	if (digit && !comma && !f)
+		return (_INT);
+	if (digit && comma && f)
+		return (_FLOAT);
+	if (digit && comma && !f)
+		return (_DOUBLE);
+	return (_STRING);
+}
+
+static int	detectType(std::string str)
 {
 	std::string::size_type	i;
-	bool					letter = false;
+	size_t					len;
 	bool					comma = false;
+	bool					digit = true;
 	bool					f = false;
-	
-	for (i = 0; i < str.length(); i++)
+
+	len = str.length();
+	for (i = 0; i < len; i++)
 	{
-		if (!letter && str[i] >= 'a' && str[i] <= 'z')
-			letter = true;
+		if (!comma && i > 0 && i != len - 1 && str[i] == ',')
+			comma = true;
+		else if (i > 0 && i == len - 1 && str[i] == 'f')
+			f = true;
+		else if (!(str[i] >= '0' && str[i] <= '9'))
+		{
+			digit = false;
+			break ;
+		}
 	}
+	return (correctType(i, comma, digit, f));
 }
 
 int	main(int argc, char **argv)
 {
 	std::string	str;
+	int			type;
 
 	if (argc != 2)
 	{
@@ -51,7 +81,7 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	str = argv[1];
-	detectType(str);
+	type = detectType(str);
 	convert(str);
 	return (0);
 }
