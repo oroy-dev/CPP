c70f0751f0cf95eb80eaaaebc08f17aeb153454f
oroy-dev <olivierroy123@gmail.com>
2024-06-12 19:39:57 -0400

    cpp08 start

---
+++ CPP-Module-08/ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 17:09:30 by oroy              #+#    #+#             */
+/*   Updated: 2024/06/12 17:15:31 by oroy             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template <typename T, int>
+class easyfind
+{
+private:
+	/* data */
+public:
+	easyfind(/* args */);
+	~easyfind();
+};
+
+# include "easyfind.tpp"
+
+#endif
---
+++ CPP-Module-08/ex00/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/12 17:08:48 by oroy              #+#    #+#             */
+/*   Updated: 2024/06/12 19:39:25 by oroy             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "easyfind.hpp"
+
+#include <iostream>
+#include <exception>
+#include <list>
+
+class	NoOccurenceFound : public std::exception
+{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("No Occurence Found");
+		}
+};
+
+// template <typename T>
+int	easyfind(std::list<int> const lst, int data)
+{
+	std::list<int>::const_iterator	it;
+	std::list<int>::const_iterator	ite = lst.end();
+	
+	for (it = lst.begin(); it != ite; ++it)
+	{
+		if (*it == data)
+			return (*it);
+	}
+	throw	NoOccurenceFound();
+}
+
+int	main(void)
+{
+	std::list<int>	lst;
+
+	lst.push_back(1);
+	lst.push_back(3);
+	lst.push_back(6);
+	lst.push_back(127);
+
+	try
+	{
+		std::cout << easyfind(lst, 42) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
+
+	return (0);	
+}
