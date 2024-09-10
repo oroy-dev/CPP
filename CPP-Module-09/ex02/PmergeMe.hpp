8f962cd08bee72156202c72d6b458bbfbdea552d
oroy-dev <olivierroy123@gmail.com>
2024-09-10 13:21:31 -0400

    ex02

--- CPP-Module-09/ex02/PmergeMe.hpp
+++ CPP-Module-09/ex02/PmergeMe.hpp
@@ -1,27 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oroy <oroy@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/19 20:01:25 by olivierroy        #+#    #+#             */
-/*   Updated: 2024/09/10 10:47:52 by oroy             ###   ########.fr       */
+/*   Created: 2023/12/13 15:50:37 by oroy              #+#    #+#             */
+/*   Updated: 2024/09/10 11:06:46 by oroy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "RPN.hpp"
-
-int	main(int argc, char **argv)
-{
-	RPN	*rpn;
-
-	if (argc != 2)
-	{
-		std::cerr << "Error: Please enter one argument" << std::endl;
-		return 1;
-	}
-	rpn = new RPN(argv[1]);
-	delete rpn;
-	return 0;
-}
